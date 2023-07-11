c7f8575791870524857d7b7ee54259ec3830ba0c
sreeharij <sreeharij2003@gmail.com>
2023-07-11 21:04:34 +0530

    implemented binary_tree in c++

---
+++ DSA_Udemy/trees/binary_tree.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Binary_Tree{
+private:
+    struct Node{
+        Node* lchild = nullptr;
+        int data;
+        Node* rchild = nullptr;
+    };
+public:    
+    Node *root;
+    Binary_Tree(){
+        root = nullptr;
+    }
+
+    void CreateTree(){
+        int x=0;
+        Node *t,*ptr;
+        queue<Node*> memory_address_queue;
+        cout<<"Enter Root value: ";
+        cin>>x;
+        t = new Node;
+        t->data = x;
+        root = t;
+        memory_address_queue.push(t);
+        
+        while(!memory_address_queue.empty()){
+            ptr = memory_address_queue.front();
+            memory_address_queue.pop();
+            cout<<"Enter left child(-1 if no left child) of "<<ptr->data<<": ";
+            cin>>x;
+            if(x!=-1){
+                t = new Node;
+                t->data = x;
+                ptr->lchild = t;
+                memory_address_queue.push(t);
+            }
+            cout<<"Enter right child(-1 if no right child) of "<<ptr->data<<": ";
+            cin>>x;
+            if(x!=-1){
+                t = new Node;
+                t->data = x;
+                ptr->rchild = t;
+                memory_address_queue.push(t);
+            }
+        }
+    }
+    void Preorder(){Preorder(root);}
+    void Preorder(Node *ptr){
+        if(ptr){
+            cout<<ptr->data<<", ";
+            Preorder(ptr->lchild);
+            Preorder(ptr->rchild);
+        }
+    }
+
+    void Inorder(){Inorder(root);}
+    void Inorder(Node *ptr){
+        if(ptr){
+            Inorder(ptr->lchild);
+            cout<<ptr->data<<", ";
+            Inorder(ptr->rchild);
+        }
+    }
+
+    void Postorder(){Postorder(root);}
+    void Postorder(Node *ptr){
+        if(ptr){
+            Postorder(ptr->lchild);
+            Postorder(ptr->rchild);
+            cout<<ptr->data<<", ";
+        }
+    }
+
+    void Levelorder(){
+        Node *ptr;
+        queue<Node*> memory_address_queue;
+        cout<<root->data<<", ";
+        memory_address_queue.push(root);
+        while(!memory_address_queue.empty()){
+            ptr = memory_address_queue.front();
+            memory_address_queue.pop();
+            if(ptr->lchild){
+                cout<<ptr->lchild->data<<", ";
+                memory_address_queue.push(ptr->lchild);
+            }
+            if(ptr->rchild){
+                cout<<ptr->rchild->data<<", ";
+                memory_address_queue.push(ptr->rchild);
+            }
+            
+        }
+    }
+
+    int Height(){return Height(root);}
+    int Height(Node *ptr){
+        int left=0,right=0;
+        if(ptr == nullptr){
+            return -1;
+        }
+        left = Height(ptr->lchild);
+        right = Height(ptr->rchild);
+
+        int current_height = left > right ? left+1 : right+1;
+        return current_height;
+    }
+
+    int Count_all_nodes(){return Count_all_nodes(root);}
+    int Count_all_nodes(Node* ptr){
+        int left = 0;
+        int right = 0;
+
+        if(ptr == nullptr){
+            return 0;
+        }
+        return Count_all_nodes(ptr->lchild) + Count_all_nodes(ptr->rchild) + 1;
+    }
+
+    int Count_leaf_nodes(){return Count_leaf_nodes(root);}
+    int Count_leaf_nodes(Node *ptr){
+        if(ptr == nullptr){
+            return 0;
+        }
+        if(ptr->lchild==nullptr && ptr->rchild==nullptr){
+            return 1;
+        }
+        return Count_leaf_nodes(ptr->lchild) + Count_leaf_nodes(ptr->rchild);
+    }
+
+    int Count_deg2_nodes(){return Count_deg2_nodes(root);}
+    int Count_deg2_nodes(Node *ptr){
+        if(ptr == nullptr){
+            return 0;
+        }
+        int x = Count_deg2_nodes(ptr->lchild);
+        int y = Count_deg2_nodes(ptr->rchild);
+        if(ptr->lchild && ptr->rchild){
+            return x+y+1;
+        }
+        return x+y;
+    }
+
+    int Count_deg1_nodes(){return Count_deg1_nodes(root);}
+    int Count_deg1_nodes(Node *ptr){
+        if(ptr == nullptr){
+            return 0;
+        }
+        int x = Count_deg1_nodes(ptr->lchild);
+        int y = Count_deg1_nodes(ptr->rchild);
+        if((ptr->lchild && ptr->rchild==nullptr) || (ptr->lchild==nullptr && ptr->rchild)){
+            //USE BOOLEAN LOGIC TO SIMPLIFY THE ABOVE CONDITION to (ptr->lchild ^ ptr->rchild) XOR operator
+            return x+y+1;
+        }
+        return x+y;
+    }
+};
+
+int main(){
+    Binary_Tree BT1;
+    BT1.CreateTree();
+    cout<<endl;
+    cout<<"Preorder: ";
+    BT1.Preorder();
+    cout<<"\nInorder: ";
+    BT1.Inorder();
+    cout<<"\nPostorder: ";
+    BT1.Postorder();
+    cout<<"\nLevelorder: ";
+    BT1.Levelorder();
+    cout<<"\nHeight: "<<BT1.Height()<<endl;
+    cout<<"\nCount_all_nodes: "<<BT1.Count_all_nodes()<<endl;
+    cout<<"\nCount_leaf_nodes: "<<BT1.Count_leaf_nodes()<<endl;
+    cout<<"\nCount_deg2_nodes: "<<BT1.Count_deg2_nodes()<<endl;
+    cout<<"\nCount_deg1_nodes: "<<BT1.Count_deg1_nodes()<<endl;
+    return 0;
+}
