28b176c87f1d267b8c95cf2b48187da6ab0446a3
Sreeharij <sreeharij2003@gmail.com>
2023-08-23 23:29:16 +0530

    simplified my solution to C - Phoenix and Puzzle

--- Competitive Programming/vjudge/contest-576381/C - Phoenix and Puzzle.py
+++ Competitive Programming/vjudge/contest-576381/C - Phoenix and Puzzle.py
@@ -1,21 +1,14 @@
-import math
-
 def check_perfect_square(num):
     return (int(num**0.5) * int(num**0.5) == num)
 
 for _ in range(int(input())):
     n = int(input())
-    if n==1 or n%2!=0:
-        print("NO")
+    flag = False
+    if check_perfect_square(n/2):
+        flag = True 
+    elif check_perfect_square(n/4):
+        flag = True 
+    if flag:
+        print("YES")
     else:
-        power_of_2 = 1
-        flag = False
-        while(power_of_2 < n):
-            power_of_2 *= 2
-            if(check_perfect_square(n/power_of_2)):
-                flag = True
-                break 
-        if flag:
-            print("YES")
-        else:
-            print("NO")
+        print("NO")
