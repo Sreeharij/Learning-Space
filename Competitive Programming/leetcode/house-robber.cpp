c2913ee19e98ac81b6f976beb4686ff42c224c67
Sreehari J <sreeharij2003@gmail.com>
2023-09-02 23:38:14 +0530

    space optimised from O(n) to O(1) for house-robber

--- Competitive Programming/leetcode/house-robber.cpp
+++ Competitive Programming/leetcode/house-robber.cpp
@@ -1,15 +1,18 @@
+//SPACE OPTIMISED SOLUTION
 class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1){
             return nums[0];
         }
-        int dp[nums.size()];
-        dp[0] = nums[0];
-        dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+        int first = nums[0];
+        int second = nums[0] > nums[1] ? nums[0] : nums[1];
+        int current = second;
         for(int i=2;i<nums.size();i++){
-            dp[i] = (dp[i-2]+nums[i]) > dp[i-1] ? (dp[i-2]+nums[i]): dp[i-1];
+            current = (first+nums[i]) > second ? (first+nums[i]): second;
+            first = second;
+            second = current;
         }
-        return dp[nums.size()-1];
+        return current;
     }
 };
