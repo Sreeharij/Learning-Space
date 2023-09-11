bca1a2a929169b9aa2e0f4c1b651705e50c94b3f
Sreehari J <sreeharij2003@gmail.com>
2023-09-11 23:01:44 +0530

    add bottom up tabulation dp solution

--- Competitive Programming/codingninjas/ninja’s-training_3621003.cpp
+++ Competitive Programming/codingninjas/ninja’s-training_3621003.cpp
@@ -1,3 +1,4 @@
+//BELOW IS TOP DOWN WITH MEMOIZATION
 int func(vector<vector<int>> &points,int n,int last,vector<vector<int>>& dp){
     int max_val = 0;
     int current_branch = 0;
@@ -28,4 +29,24 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     vector<vector<int>> dp(n,vector<int>(4,-1)); 
     int answer = func(points,n-1,3,dp);
     return answer;
+}
+
+//BELOW IS BOTTOM UP TABULATION
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    int dp[n][3];
+
+    dp[0][0] = points[0][0];
+    dp[0][1] = points[0][1];
+    dp[0][2] = points[0][2];
+    
+    for(int i=1;i<n;i++){
+        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + points[i][0];
+        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + points[i][1];
+        dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + points[i][2];
+    }
+    int max_val = dp[n-1][0] > dp[n-1][1] ? dp[n-1][0] : dp[n-1][1];
+    max_val = max_val > dp[n-1][2] ? max_val : dp[n-1][2];
+
+    return max_val;
 }
