7f0538678a5852a806f070eb8b4fc6f366e2d428
sreeharij <sreeharij2003@gmail.com>
2024-01-04 12:13:21 +0530

    added bfs solution to find depth of binary tree

--- Competitive Programming/neetcode/maximum-depth-of-binary-tree.cpp
+++ Competitive Programming/neetcode/maximum-depth-of-binary-tree.cpp
@@ -1,3 +1,5 @@
+//SOLUTION USING DEPTH FIRST SEARCH APPROACH GIVEN BELOW
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -19,5 +21,38 @@ public:
         int y = maxDepth(root->right);
         int max_height  = x > y ? x : y;
         return max_height+1;
+    }
+};
+
+
+
+
+
+//SOLUTION USING BREAD FIRST SEARCH GIVEN APPROACH BELOW
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        queue<TreeNode* > q;
+        TreeNode* ptr;
+        int level = 0;
+        if(root){
+            q.push(root);
+        }
+        int current_queue_size;
+        while(!q.empty()){
+            current_queue_size = q.size();
+            for(int i=0;i<current_queue_size;i++){
+                ptr = q.front();
+                q.pop();
+                if(ptr->left){
+                    q.push(ptr->left);
+                }
+                if(ptr->right){
+                    q.push(ptr->right);
+                }
+            }
+            level += 1;
+        }
+        return level;
     }
 };
