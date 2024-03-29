34643b72742b619759e874a625c84bbaff3818e0
sreeharij <sreeharij2003@gmail.com>
2024-03-29 22:41:32 +0530

    avl tree partial

---
+++ DSA/trees/avl_tree.cpp
@@ -0,0 +1,29 @@
+#include <iostream>>
+
+class AVL_Tree{
+private:
+    struct Node{
+        Node* lchild = nullptr;
+        Node* rchild = nullptr;
+        int data;
+    }; 
+
+public:
+    Node* root;
+    AVL_Tree(){root = nullptr;}
+    ~AVL_Tree(){}
+
+    Node* recursive_insert(Node* ptr,int key){
+        if(!ptr){
+            ptr = new Node();
+        }
+    }
+
+    Node * 
+};
+
+int main(){
+
+
+    return 0;
+}
