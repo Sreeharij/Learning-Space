d14f4afe4cad6133353aec8b5e344f4873d6fa04
sreeharij <sreeharij2003@gmail.com>
2024-05-09 11:40:48 +0530

    added bfs sol to islands and tressure

--- Competitive Programming/neetcode/islands-and-treasure.cpp
+++ Competitive Programming/neetcode/islands-and-treasure.cpp
@@ -1,14 +1,14 @@
+//DFS APPROACH BELOW
 class Solution {
 public:
-    bool is_valid(int x,int y,int rows,int cols,vector<vector<int>>& grid,vector<vector<bool>>& visited){
+    bool is_valid(int x,int y,int rows,int cols,vector<vector<int>>& grid){
         if(x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] != -1){
             return true;
         }
         return false;
     }
 
-    void dfs(int x,int y,int rows,int cols,vector<vector<bool>>& visited,vector<vector<int>>& grid,int distance){
-        visited[x][y] = true;
+    void dfs(int x,int y,int rows,int cols,vector<vector<int>>& grid,int distance){
         if(grid[x][y] == 0 && distance!=0){
             return;
         }
@@ -18,17 +18,17 @@ public:
         else{
             return;
         }
-        if(is_valid(x+1,y,rows,cols,grid,visited)){
-            dfs(x+1,y,rows,cols,visited,grid,grid[x][y]+1);
+        if(is_valid(x+1,y,rows,cols,grid)){
+            dfs(x+1,y,rows,cols,grid,grid[x][y]+1);
         }
-        if(is_valid(x-1,y,rows,cols,grid,visited)){
-            dfs(x-1,y,rows,cols,visited,grid,grid[x][y]+1);
+        if(is_valid(x-1,y,rows,cols,grid)){
+            dfs(x-1,y,rows,cols,grid,grid[x][y]+1);
         }
-        if(is_valid(x,y+1,rows,cols,grid,visited)){
-            dfs(x,y+1,rows,cols,visited,grid,grid[x][y]+1);
+        if(is_valid(x,y+1,rows,cols,grid)){
+            dfs(x,y+1,rows,cols,grid,grid[x][y]+1);
         }
-        if(is_valid(x,y-1,rows,cols,grid,visited)){
-            dfs(x,y-1,rows,cols,visited,grid,grid[x][y]+1);
+        if(is_valid(x,y-1,rows,cols,grid)){
+            dfs(x,y-1,rows,cols,grid,grid[x][y]+1);
         }
     }      
 
@@ -45,8 +45,54 @@ public:
         }
         for(auto x: tressure){
             int distance = 0;
-            vector<vector<bool>> visited(rows,vector<bool>(cols,false));
-            dfs(x.first,x.second,rows,cols,visited,grid,distance);
+            dfs(x.first,x.second,rows,cols,grid,distance);
+        }
+    }
+};
+
+
+
+//BFS APPROACH BELOW
+class Solution {
+public:
+    bool is_valid(int x,int y,int rows,int cols,vector<vector<int>>& grid){
+        if(x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == INT_MAX){
+            return true;
+        }
+        return false;
+    }  
+
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int,int>> q;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 0){
+                    q.push({i,j});
+                }
+            }
+        }
+        while(!q.empty()){
+            int x_coord = q.front().first;
+            int y_coord = q.front().second;
+            q.pop();
+            if(is_valid(x_coord+1,y_coord,rows,cols,grid)){
+                grid[x_coord+1][y_coord] = grid[x_coord][y_coord] + 1;
+                q.push({x_coord+1,y_coord});
+            }
+            if(is_valid(x_coord-1,y_coord,rows,cols,grid)){
+                grid[x_coord-1][y_coord] = grid[x_coord][y_coord] + 1;
+                q.push({x_coord-1,y_coord});
+            }
+            if(is_valid(x_coord,y_coord+1,rows,cols,grid)){
+                grid[x_coord][y_coord+1] = grid[x_coord][y_coord] + 1;
+                q.push({x_coord,y_coord+1});
+            }
+            if(is_valid(x_coord,y_coord-1,rows,cols,grid)){
+                grid[x_coord][y_coord-1] = grid[x_coord][y_coord] + 1;
+                q.push({x_coord,y_coord-1});
+            }
         }
     }
 };
