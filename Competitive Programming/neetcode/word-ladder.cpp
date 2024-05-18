9153fff836c59ed05a363d41ebbcc54a75ba6b7c
sreeharij <sreeharij2003@gmail.com>
2024-05-18 10:48:25 +0530

    optimised word ladder solution

--- Competitive Programming/neetcode/word-ladder.cpp
+++ Competitive Programming/neetcode/word-ladder.cpp
@@ -64,3 +64,76 @@ public:
         return shortest_length;
     }
 };
+
+//below implementation takes O(n*m) for making patterns_map and O(n^2) for bfs = O(n^2) total complexity where n is length of wordList
+class Solution {
+public:
+    void get_patterns_generated(string& word, unordered_map<string,vector<string>>& patterns_map,int word_length){
+        string key = word;
+        for(int i=0;i<word_length;i++){
+            key[i] = '*';
+            patterns_map[key].push_back(word);
+            key[i] = word[i];
+        }
+    }
+
+    int get_shortest_distance(string& beginWord,string& endWord,unordered_map<string,vector<string>>& graph){
+        if(graph.find(endWord) == graph.end()){
+            return 0;
+        }
+        unordered_map<string,int> visited;
+        for(auto x: graph){
+            visited[x.first] = -1;
+        }
+        queue<string> q;
+        q.push(beginWord);
+        visited[beginWord] = 1;
+
+        while(!q.empty()){
+            string current_word = q.front();
+            q.pop();
+            for(auto word: graph[current_word]){
+                if(visited[word] == -1){
+                    visited[word] = visited[current_word] + 1;
+                    q.push(word);
+                }
+            }
+        }
+        if(visited[endWord] == -1) return 0;
+        return visited[endWord];
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        bool present = false;
+        for(auto word: wordList){
+            if(word == endWord){
+                present=true;
+                break;
+            }
+        }
+        if(!present) return 0;
+
+        int shortest_length = 0;
+        unordered_map<string,vector<string>> graph;
+        int word_length = beginWord.size();
+        int wordlist_size = wordList.size();
+        
+        unordered_map<string,vector<string>> patterns_map;
+        for(int i=0;i<wordlist_size;i++){
+            get_patterns_generated(wordList[i],patterns_map,word_length);
+        }
+        get_patterns_generated(beginWord,patterns_map,word_length);
+        for(auto pair: patterns_map){
+            int length = pair.second.size();
+            for(int i=0;i<length;i++){
+                for(int j=i+1;j<length;j++){
+                    graph[pair.second[i]].push_back(pair.second[j]);
+                    graph[pair.second[j]].push_back(pair.second[i]);
+                }
+            }
+        }
+        shortest_length = get_shortest_distance(beginWord,endWord,graph);
+        return shortest_length;
+        return 10;
+    }
+};
