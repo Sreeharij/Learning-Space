5f00349d829791cf316671861f88d8f32c9d61d0
sreeharij <sreeharij2003@gmail.com>
2024-05-20 12:12:47 +0530

    sudoku solver optimised

--- Competitive Programming/leetcode/sudoku-solver.cpp
+++ Competitive Programming/leetcode/sudoku-solver.cpp
@@ -1,24 +1,22 @@
-class Solution {
+class Solution{
+private:
+    bool rows[9][9] = {false};
+    bool cols[9][9] = {false};
+    bool box[9][9] = {false};
+    bool PLACE = true;
+    bool REMOVE = false;
 public:
     bool is_valid(vector<vector<char>>& board,int x,int y,char val){
-        for(int i=0;i<9;i++){
-            if(board[x][i] == val){
-                return false;
-            }
-            if(board[i][y] == val){
-                return false;
-            }
-        }
-        int box_x_start = x/3 * 3;
-        int box_y_start = y/3 * 3;
-        for(int i=box_x_start;i<box_x_start+3;i++){
-            for(int j=box_y_start;j<box_y_start+3;j++){
-                if(board[i][j] == val){
-                    return false;
-                }
-            }
-        }
-        return true;
+        int number = val - '1';
+        return !(rows[x][number] || cols[y][number] || box[x/3 * 3 + y/3][number]);
+    }
+
+    void put_value(vector<vector<char>>& board,int x,int y,char val,bool FLAG){
+        int number = val - '1';
+        board[x][y] = FLAG ? val : '.';
+        rows[x][number] = FLAG;
+        cols[y][number] = FLAG;
+        box[x/3 * 3 + y/3][number] = FLAG;
     }
 
     bool backtrack(vector<vector<char>>& board,vector<pair<int,int>>& initial_empty_cells,int idx){
@@ -29,11 +27,11 @@ public:
         int y = initial_empty_cells[idx].second;
         for(char val='1';val<='9';val++){
             if(is_valid(board,x,y,val)){
-                board[x][y] = val;
+                put_value(board,x,y,val,PLACE);
                 if(backtrack(board,initial_empty_cells,idx+1)){
                     return true;
                 }
-                board[x][y] = '.';
+                put_value(board,x,y,val,REMOVE);
             }
         }
         return false;
@@ -45,6 +43,12 @@ public:
             for(int j=0;j<9;j++){
                 if(board[i][j] == '.'){
                     initial_empty_cells.push_back({i,j});
+                }
+                else{
+                    int number = board[i][j] - '1';
+                    rows[i][number] = true;
+                    cols[j][number] = true;
+                    box[i/3 * 3 + j/3][number] = true;
                 }
             }
         }
