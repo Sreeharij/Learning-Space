6c99b6c143bb8b640aecd06bb17ec25ae772c721
sreeharij <sreeharij2003@gmail.com>
2024-05-27 10:42:51 +0530

    learning js

---
+++ web dev/simple_js.js
@@ -0,0 +1,17 @@
+let state = 0;
+function toggle(){
+    var x = document.getElementById("demo");
+    state = !state;
+    x.innerHTML = state;
+    console.log("Ran toggle() function");
+}
+// var username = window.prompt("Enter your name: ");
+// if(username != null){
+//     window.alert("Hello "+username);
+// }
+// else{
+//     window.alert("You cancelled the prompt.");
+// }
+let x = 10;
+console.log(`value of x is ${x}`);
+
