c8ba5c26545fc3d77efbb527f8f903e483a6d91c
sreeharij <sreeharij2003@gmail.com>
2024-06-05 15:28:07 +0530

    request headers

---
+++ app/nextjs-app/my-app/src/app/profile/route.tsx
@@ -0,0 +1,14 @@
+import type { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest){
+    const headerList = headers();
+    return new Response("<h1>Reponse </h1>",{
+        headers:
+        {
+            "Content-Type" : "text/html",
+            
+        },
+    });
+}
+
