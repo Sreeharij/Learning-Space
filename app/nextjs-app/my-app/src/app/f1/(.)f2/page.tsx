589114f154e6073adc1a58ce2993b546de8a00b5
sreeharij <sreeharij2003@gmail.com>
2024-06-06 13:20:50 +0530

    multilevel intercepting routes

---
+++ app/nextjs-app/my-app/src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link"
+export default function f2_intercepted(){
+    return (
+    <>
+        <h1>(.)Intercepted f2 page</h1> 
+    </>
+    );
+}
---
+++ app/nextjs-app/my-app/src/app/f1/f2/(..)f3/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link"
+export default function intercepted_f2(){
+    return (
+    <>
+        <h1>(..) Intercepted f3 Page from f2</h1>
+    </>
+    );
+}
