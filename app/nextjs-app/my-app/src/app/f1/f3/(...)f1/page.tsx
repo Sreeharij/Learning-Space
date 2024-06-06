15020d31d864b8d148011bcbad41e2df3779b51e
sreeharij <sreeharij2003@gmail.com>
2024-06-06 13:21:04 +0530

    intercepting routes

---
+++ app/nextjs-app/my-app/src/app/f1/f3/(...)f1/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function intercepted_f1(){
+    return(
+        <>
+            <h1>(...) Intercepted F1 Page from F3</h1>
+        </>
+    );
+}
---
+++ app/nextjs-app/my-app/src/app/f1/f3/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function f3(){
+    return(
+        <>
+            <h1>F3 Page</h1>
+            <Link href="/f1">Go to F1</Link>
+        </>
+    );
+}
