b0377e58688510ecee2e933bd9eb8af10d8b652a
sreeharij <sreeharij2003@gmail.com>
2024-10-20 11:34:54 +0530

    learned -sp -a REPLY

--- shell/lec2.sh
+++ shell/lec2.sh
@@ -10,4 +10,16 @@ echo $PWD
 intro_message="Hello From the Other Side"
 
 echo $intro_messagea
-echo this is a message
+
+read -p "Username: " username
+read -sp "Password: " passwd
+echo "Your username is $username and password is $passwd"
+
+echo "Enter names: "
+read -a names
+echo ${names[2]}
+echo $names
+
+echo "Enter stuff to be stored in REPLY variable: "
+read
+echo $REPLY
