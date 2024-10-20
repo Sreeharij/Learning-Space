987c12015e089555ef5117e9b9dcd5ec3e87b265
sreeharij <sreeharij2003@gmail.com>
2024-10-20 11:21:49 +0530

    add lec2

---
+++ shell/lec2.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#this is a comment
+
+echo $BASH
+echo $HOME
+echo $BASH_VERSION
+echo $PWD
+
+intro_message="Hello From the Other Side"
+
+echo $intro_messagea
+echo this is a message
