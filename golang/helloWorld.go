cb4df4098b2b70cd3f9c443163172d116bc68462
sreeharij <sreeharij2003@gmail.com>
2025-05-12 14:35:15 +0530

    switch statement

--- golang/helloWorld.go
+++ golang/helloWorld.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func checkType(i interface{}){
+	switch v := i.(type) {
+    case int:
+        fmt.Println("It's an int:", v)
+    case string:
+        fmt.Println("It's a string:", v)
+    default:
+        fmt.Printf("Unknown type %T\n", v)
+    }
+}
+
 func main(){
 	fmt.Println("Hello World")
+	// fmt.Println("Statement 2")
+	num := 100
+
+	checkType(num);
+	
+	
 }
