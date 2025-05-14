12a88ff28d311dd974261c413f7c2b8fa61f8730
sreeharij <sreeharij2003@gmail.com>
2025-05-14 21:24:14 +0530

    mutex

--- golang/helloWorld.go
+++ golang/helloWorld.go
@@ -1,24 +1,48 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func checkType(i interface{}){
+func checkType(i interface{}) {
 	switch v := i.(type) {
-    case int:
-        fmt.Println("It's an int:", v)
-    case string:
-        fmt.Println("It's a string:", v)
-    default:
-        fmt.Printf("Unknown type %T\n", v)
-    }
+	case int:
+		fmt.Println("It's an int:", v)
+	case string:
+		fmt.Println("It's a string:", v)
+	default:
+		fmt.Printf("Unknown type %T\n", v)
+	}
 }
 
-func main(){
-	fmt.Println("Hello World")
-	// fmt.Println("Statement 2")
-	num := 100
+func main() {
 
-	checkType(num);
-	
-	
+	messages := make(chan string)
+	defer checkType(messages)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		messages <- "Hello From first goroutine!"
+	}()
+	fmt.Println("Main here")
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d\n", i)
+	}
+	msg := <-messages
+	fmt.Println(msg)
+
+	if 5 == 45 {
+		fmt.Println("5 == 5")
+	} else {
+		fmt.Println("Else part")
+	}
+
+	var mp = map[int]int{
+		1: 10,
+		2: 20,
+		3: 30,
+	}
+	fmt.Println(mp)
+
 }
