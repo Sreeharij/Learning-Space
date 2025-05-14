e9dd4cbd92db54ed765363db14cf6b54effea2e9
sreeharij <sreeharij2003@gmail.com>
2025-05-14 11:43:52 +0530

    passing values to function closures

--- golang/createSimpleClosure.go
+++ golang/createSimpleClosure.go
@@ -13,8 +13,21 @@ func main(){
 		}
 	}
 
+	updateWithInitialValue := func(initial int) func() int{
+		count := initial
+
+		return func() int {
+			count++
+			return count
+		}
+	}
+
 	increment := updateCounter()
 	
 	fmt.Println(increment())
 	fmt.Println(increment())
+
+	inc_x := updateWithInitialValue(200)
+	fmt.Println(inc_x())
+	fmt.Println(inc_x())
 }
