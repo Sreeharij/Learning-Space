a582891a29bf3aad072459152c07bfa45a2edc98
sreeharij <sreeharij2003@gmail.com>
2026-02-03 19:58:32 +0530

    framer motion transition

--- framer_motion/framer-motion-basics/src/app/page.tsx
+++ framer_motion/framer-motion-basics/src/app/page.tsx
@@ -5,14 +5,16 @@ export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 200 }}
+        initial={{ opacity: 0, x: -100, y: 50 }}
+        animate={{ opacity: 1, x: 200, y: 300 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
         style={{
           width: 100,
           height: 100,
           background: "white",
         }}
       />
+
     </div>
   );
 }
