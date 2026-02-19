ba35b62529b70da3ba5a770dd8206088d0ad959f
sreeharij <sreeharij2003@gmail.com>
2026-02-19 18:22:02 +0530

    setting hostname, new pid, uts, mount space

--- containerisation_in_linux/main.go
+++ containerisation_in_linux/main.go
@@ -4,19 +4,43 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
-		panic("usage: run <command>")
+		panic("bad command")
 	}
 }
 
 func run() {
+	// Re-run this binary, but call the "child" case
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	// CLONE_NEWUTS: Isolate Hostname
+	// CLONE_NEWPID: Isolate Process IDs (Child becomes PID 1)
+	// CLONE_NEWNS:  Isolate Mount points
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+func child() {
 	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	// Inside the child, we can set a private hostname
+	syscall.Sethostname([]byte("container-demo"))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
